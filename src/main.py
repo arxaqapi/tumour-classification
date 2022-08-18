7d8ea774975be4ccfa84aa951ddc3eac0f26eb6b
arxaqapi <arxaqapi@protonmail.com>
2022-08-18 16:05:28 +0200

    feat: cleaned up main

--- src/main.py
+++ src/main.py
@@ -1,26 +1,7 @@
-# from IsotopeNet import IsotopeNet
-# from ResidualNet import ResidualNet
-from AutoEncoder import train_ae, train_convae
 from DatasetGeneration import TMADataset
-import numpy as np
-# from keras.losses import SparseCategoricalCrossentropy
-# from tensorflow.keras.optimizers import Adam
-
-
-# OPTIM = Adam(learning_rate=0.01)
-# LOSS = SparseCategoricalCrossentropy()
 
 
 if __name__ == '__main__':
-  # input_shape = (None, 4672, 1, 1)
-  # resnet_model = ResidualNet()
-  # resnet_model.build(input_shape=input_shape)
-  # resnet_model.summary()
-
-  # resnet_model.compile(
-  #   optimizer=OPTIM,
-  #   loss=LOSS
-  # )
 
   dataset = TMADataset()
   dataset.bounding_boxes_of_tissue_samples()
@@ -31,13 +12,3 @@ if __name__ == '__main__':
     overwrite=False
   )
 
-  print(X_train.shape)
-  print(X_test.shape)
-  print(y_train[:10])
-
-  # train_ae(X_train, X_test, y_train, y_test)
-  train_convae(
-    X_train,
-    X_test,
-    y_train,
-    y_test)
